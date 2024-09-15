a025212aa4d5b67990a9ee5d3b3f9846c9e7869c
Gareth Lloyd <gareth@ignition-web.co.uk>
2024-09-15 16:31:36 +0100

    Initial save/load

--- frontend/svelte/src/routes/+page.svelte
+++ frontend/svelte/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import LocalStorage from '$lib/components/LocalStorage.svelte';
+
+    let currentValue: string = "";
+
+    function getValue() {
+        return currentValue;
+    }
+
+    function setValue(value: string) {
+        console.log(value)
+        currentValue = value;
+    }
+</script>
+<!--
+<h2>Current Value</h2>
+<p>{currentValue}</p>
+-->
+
+<div>
+    <label for="name">Value: </label>
+    <input id="name" bind:value={currentValue} placeholder="Enter some text" />
+</div>
+
+<!-- Use the generic component -->
+<LocalStorage {getValue} {setValue} keyPrefix="data-" />
